1d064e480414a134e508d32c7ca793cd000d1bfc
john <one23four5ate9@yahoo.com>
2019-02-03 03:54:06 -0500

    yes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
   $("#buzzbutton").on("click", function() {
 
 
-    var queryURL = "http://69.113.126.184:5050/?TEST&BLINK";
+    var queryURL = process.env.API;
     
     $.ajax({
          url: queryURL,
